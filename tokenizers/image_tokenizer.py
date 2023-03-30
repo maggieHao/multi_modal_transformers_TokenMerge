24273183b1faa6c40b5e3bad8ee620b84740a46f
peterdavidfagan <peterdavidfagan@gmail.com>
2023-03-30 17:54:00 +0100

    adding tokenizers

--- tokenizers/image_tokenizer.py
+++ tokenizers/image_tokenizer.py
@@ -1,7 +1,8 @@
-"""
-A set of image tokenizers for use with MART.
-"""
+"""Image Tokenizer Implementation"""
+
 import warnings
+from functools import partial
+from typing import Any, Callable, Sequence, Tuple
 
 import einops
 import chex
@@ -9,12 +10,14 @@ import jax
 import jax.numpy as jnp
 import flax.linen as nn
 
-
-########################
-# Helper Functions
-########################
+ModuleDef = Any
 
 
+############################
+# Image Patching Functions
+############################
+
+# TODO(peterdavidfagan): verify this is row-major format.
 def image_to_patches(image, patch_size, normalize):
     """
     Converts an image into patches, assuming square images.
@@ -32,6 +35,7 @@ def image_to_patches(image, patch_size, normalize):
     chex.assert_equal(h, w)
 
     # check if the image is divisible by the patch size
+    # automatically resize if not
     if h % patch_size != 0:
         warnings.warn(
             "The image is not divisible by the patch size. Automatically resizing image."
@@ -41,7 +45,7 @@ def image_to_patches(image, patch_size, normalize):
 
     # create an array of patches
     patches = einops.rearrange(
-        image, "c (h p1) (w p2) -> (h w) (p1 p2 c)", p1=patch_size, p2=patch_size
+        image, "c (h p1) (w p2) -> (h w) (p1) (p2) (c)", p1=patch_size, p2=patch_size
     )
 
     # normalize pixel values
@@ -51,14 +55,12 @@ def image_to_patches(image, patch_size, normalize):
 
     return patches
 
-
 # vectorise image patching function
 image_to_patches_v = jax.vmap(image_to_patches, in_axes=(0, None, None), out_axes=(0))
 
 ########################
 # Position Encodings
 ########################
-
 
 class PositionEncoder(nn.Module):
     """
@@ -87,46 +89,102 @@ class PositionEncoder(nn.Module):
         return self.encode_mat[:, : sequence.shape[1], :]
 
 
-# TODO: Create learnable position encoding scheme
-
-
 ########################
 # Tokenizers
 ########################
 
+# use resnet for patch embedding as in Gato paper.
+# https://github.com/google/flax/blob/main/examples/imagenet/models.py
 
-class ViTTokenizer(nn.Module):
+class ResNetV2Block(nn.Module):
     """
-    Generating image tokens in accordance with ViT (https://arxiv.org/abs/2010.11929).
+    Note: fixing parameter defaults to match Gato.
+    """
+    features: int
+    strides: Tuple[int, int] = (1, 1)
+    kernel_size: Tuple[int, int] = (3, 3)
+    padding: str = "SAME"
+    weights: ModuleDef = nn.Conv
+    normalization: ModuleDef = nn.GroupNorm
+    activation: Callable = nn.gelu
+
+    @nn.compact
+    def __call__(self, x):
+        # Important: I am uncertain about this function.
+        #residual = x
+
+        # its not possible to perform group norm with 32 groups on image
+        # containing only 3 channels! Start with conv before first group norm.
+        y = self.weights(features=self.features,
+                kernel_size=(1, 1),
+                strides=self.strides,
+                padding=self.padding)(x)
+        
+        residual = y
+
+        y = self.normalization()(y)
+        y = self.activation(y)
+        y = self.weights(features=self.features, 
+                kernel_size=self.kernel_size, 
+                strides=self.strides, 
+                padding=self.padding)(y)
+
+        y = self.normalization()(y)
+        y = self.activation(y)
+        y = self.weights(features=self.features, 
+                kernel_size=self.kernel_size, 
+                strides=self.strides, 
+                padding=self.padding)(y)
+
+        return y+residual
+
+
+class ImageTokenizer(nn.Module):
+    """
+    Tokenizer for images.
     """
 
     config: dict
-    dtype = jnp.float32
+
+    @property
+    def tokens_per_image(self):
+        raise NotImplementedError
 
     def setup(self):
-        self.embed_patch = nn.Dense(features=self.config["embedding_dim"])
-        self.embed_position = PositionEncoder(self.config)
+        self.patch_size = self.config["patch_size"]
+        self.normalize = self.config["normalize"]
+        self.embedding_function = ResNetV2Block(features = self.config["embedding_dim"])
+        #self.position_encoder = PositionEncoder(self.config)
 
     def __call__(self, image):
+        # convert image into patches
+        patches = image_to_patches_v(image, self.patch_size, self.normalize)
+        
+        # create embeddings using ResNetV2
+        patches = patches.reshape(-1, self.patch_size, self.patch_size, 3)
+        embedding = self.embedding_function(patches) 
+        print(embedding.shape)
+        embedding = embedding.reshape(-1, 20, self.config["embedding_dim"])
 
-        # convert the image into image patches
-        image_patches = image_to_patches_v(image, self.config["patch_size"], True)
-
-        # generate image patch embeddings
-        image_patch_embeddings = self.embed_patch(image_patches)
-
-        # add position embeddings
-        position_embedding = self.embed_position()
-        image_patch_embeddings = image_patch_embeddings + position_embedding
-
-        return image_patch_embeddings
+        return embedding
 
 
 if __name__ == "__main__":
     # for debugging purposes
-    config = {"image_shape": (224, 224), "patch_size": 16}
+    image = jnp.ones((15, 3, 280, 280))
+    image_tokens = image_to_patches_v(image, 14, True)
 
-    tokenizer = ViTTokenizer(config)
-    image = jnp.ones((1, 3, 224, 224))
-    image_tokens = tokenizer(image)
+    # instantiate tokenizer
+    config = {
+        "patch_size": 14,
+        "normalize": True,
+        "embedding_dim": 64,
+            }
+    tokenizer = ImageTokenizer(config)
+
+    # initialize tokenizer
+    rng = jax.random.PRNGKey(0)
+    rng, init_rng = jax.random.split(rng)
+    params = tokenizer.init(init_rng, image)
+    print(params)
     print(image_tokens.shape)
---
+++ tokenizers/tensor_tokenizer.py
@@ -0,0 +1,9 @@
+"""Implementation of tokenizer for actions."""
+
+class DiscreteValueTokenizer():
+
+class ContinuousValueTokenizer():
+
+
+if __name__ == "__main__":
+    print("Testing tensor tokenizer...")
