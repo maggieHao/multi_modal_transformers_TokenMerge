2e1e1ddffda63adc20232d89b248e311d57be208
peterdavidfagan <peterdavidfagan@gmail.com>
2023-03-31 17:45:47 +0100

    checkpoint

--- models.py
+++ models.py
@@ -8,6 +8,61 @@ import chex
 import flax.linen as nn
 
 
+###########################
+# Embeddings
+###########################
+
+class ResNetV2Block(nn.Module):
+    """
+    Note: fixing parameter defaults to match Gato.
+    """
+    features: int
+    strides: Tuple[int, int] = (1, 1)
+    kernel_size: Tuple[int, int] = (3, 3)
+    padding: str = "SAME"
+    weights: ModuleDef = nn.Conv
+    normalization: ModuleDef = nn.GroupNorm
+    activation: Callable = nn.gelu
+
+    @nn.compact
+    def __call__(self, x):
+        # Important: I am uncertain about this function.
+        #residual = x
+
+        # its not possible to perform group norm with 32 groups on image
+        # containing only 3 channels! Start with conv before first group norm.
+        y = self.weights(features=self.features,
+                kernel_size=(1, 1),
+                strides=self.strides,
+                padding=self.padding)(x)
+        
+        residual = y
+
+        y = self.normalization()(y)
+        y = self.activation(y)
+        y = self.weights(features=self.features, 
+                kernel_size=self.kernel_size, 
+                strides=self.strides, 
+                padding=self.padding)(y)
+
+        y = self.normalization()(y)
+        y = self.activation(y)
+        y = self.weights(features=self.features, 
+                kernel_size=self.kernel_size, 
+                strides=self.strides, 
+                padding=self.padding)(y)
+
+        return y+residual
+
+
+class TokenLearner(nn.Module):
+    raise NotImplementedError
+
+###########################
+# Decoder-only Transformer
+###########################
+
+
 class MLPBlock(nn.Module):
     config: dict
 
@@ -36,67 +91,17 @@ class MLPBlock(nn.Module):
         return outputs
 
 
-class EncoderBlock(nn.Module):
+class DecoderBlock(nn.Module):
     config: dict
 
     @nn.compact
     def __call__(self, inputs):
         attention_config = self.config["self_attention_block"]
-        mlp_config = self.config["mlp_block"]
-
-        # Attention Block
-        x = nn.LayerNorm()(inputs)
-        x = nn.SelfAttention(
-            num_heads=attention_config["num_heads"],
-            qkv_features=attention_config["qkv_features"],
-            kernel_init=nn.initializers.normal(),
-            bias_init=nn.initializers.constant(0.0),
-            use_bias=attention_config["use_bias"],
-            broadcast_dropout=attention_config["broadcast_dropout"],
-            dropout_rate=attention_config["dropout_rate"],
-            deterministic=attention_config["deterministic"],
-        )(x)
-        x = nn.Dropout(attention_config["dropout_rate"])(x, deterministic=False)
-        # residual connection
-        x = x + inputs
-
-        # Feed Forward Block
-        y = nn.LayerNorm()(x)
-        y = MLPBlock(config=mlp_config)(y)
-        # residual connection
-        outputs = y + inputs
-
-        return outputs
-
-
-class DecoderBlock(nn.Module):
-    config: dict
-
-    @nn.compact
-    def __call__(self, encoder_outputs, decoder_inputs):
-        attention_config = self.config["self_attention_block"]
         dot_attention_config = self.config["multihead_dot_product_attention_block"]
         mlp_config = self.config["mlp_block"]
 
-        # Decoder Block
-        x = nn.LayerNorm()(decoder_inputs)
-        x = nn.SelfAttention(
-            num_heads=attention_config["num_heads"],
-            qkv_features=attention_config["qkv_features"],
-            kernel_init=nn.initializers.normal(),
-            bias_init=nn.initializers.constant(0.0),
-            use_bias=attention_config["use_bias"],
-            broadcast_dropout=attention_config["broadcast_dropout"],
-            dropout_rate=attention_config["dropout_rate"],
-            deterministic=attention_config["deterministic"],
-        )(x)
-        x = nn.Dropout(attention_config["dropout_rate"])(x, deterministic=False)
-        # residual connection
-        x = x + decoder_inputs
-
         # Encoder-Decoder Attention Block
-        y = nn.LayerNorm()(x)
-        y = nn.MultiHeadDotProductAttention(
+        x = nn.MultiHeadDotProductAttention(
             num_heads=dot_attention_config["num_heads"],
             qkv_features=dot_attention_config["qkv_features"],
             kernel_init=nn.initializers.normal(),
@@ -105,42 +110,56 @@ class DecoderBlock(nn.Module):
             broadcast_dropout=dot_attention_config["broadcast_dropout"],
             dropout_rate=dot_attention_config["dropout_rate"],
             deterministic=dot_attention_config["deterministic"],
-        )(y, encoder_outputs)
-        y = nn.Dropout(attention_config["dropout_rate"])(y, deterministic=False)
+            decode=True,
+        )(inputs_q=inputs, inputs_kv=inputs)
+        x = nn.Dropout(attention_config["dropout_rate"])(x, deterministic=False)
         # residual connection
-        y = y + x
+        x = x + inputs
 
         # Feed Forward Block
-        z = nn.LayerNorm()(y)
-        z = MLPBlock(config=mlp_config)(z)
+        y = nn.LayerNorm()(x)
+        y = MLPBlock(config=mlp_config)(y)
         # residual connection
-        outputs = z + y
+        outputs = y + x
 
         return outputs
 
 
-class Encoder(nn.Module):
-   """Encoder that accepts input embeddings"""
-
+class ConceptLearner(nn.Module):
+    """A multi-modal decoder-only Transformer architecture."""
     config: dict
 
-    def __call__(self, inputs):
-        x = inputs
-        for lyr in range(self.config["num_layers"]):
-            x = EncoderBlock(config=config)(x)
+    def __call__(self, input_seq):
+        # split the input sequence into its components
+        instruction_seq, image_seq, action_seq, concept_seq = input_seq
+
+        # generate token embeddings and add positional encoding
+        # TODO: add positional encoding
+        
+        ## instruction embeddings
+        instruction_embeddings = nn.Embed()(instruction_seq)
+
+        ## trajectory embeddings
+        image_embeddings = ResNetV2Block()(image_seq)
+        image_embeddings = TokenLearner()(image_embeddings)
+        action_embeddings = nn.Embed()(action_seq)
+
+        trajectory_embeddings = 
+
+        ## concept embeddings
+        concept_embeddings = nn.Embed()(concept_seq)
+        
+
+        embeddings = jnp.concatenate([instruction_embeddings, trajectory_embeddings, concept_embeddings], axis=1)
+
+        # pass embeddings through transformer blocks
+        for lyr in range(self.config["num_blocks"]):
+            x = DecoderBlock(config=config)(x)
+
+        # pass through final layer norm
         outputs = nn.LayerNorm()(x)
 
-        return outputs
-
-
-class Decoder(nn.Module):
-    config: dict
-
-    def __call__(self, encoder_outputs, decoder_inputs):
-        x = decoder_inputs
-        for lyr in range(self.config["num_layers"]):
-            x = DecoderBlock(config=config)(encoder_outputs, x)
-        outputs = nn.LayerNorm()(x)
+        # this should be a distribution over all tokens
         logits = nn.Dense()(outputs)
 
         return logits 
