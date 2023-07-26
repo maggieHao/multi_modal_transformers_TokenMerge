c9031f3f7847dc56409957dac7bc6d4d1f5a0f61
peterdavidfagan <peterdavidfagan@gmail.com>
2023-07-26 12:37:42 +0100

    reformatting

--- transformer_components.py
+++ transformer_components.py
@@ -1,21 +1,24 @@
 """
 Transformer architecture implementation.
+
 Heavily inspired by: https://github.com/google/flax/blob/main/examples/wmt/models.py
 """
 
-import jax
-import chex
 import flax.linen as nn
 
 ###########################
 # Decoder-only Transformer
 ###########################
 
+
 class MLPBlock(nn.Module):
+    """Transformer MLP / feed-forward block."""
+
     config: dict
 
     @nn.compact
     def __call__(self, inputs):
+        """Apply MLPBlock module."""
         x = nn.Dense(
             features=self.config["hidden_size"],
             use_bias=self.config["use_bias"],
@@ -33,7 +36,7 @@ class MLPBlock(nn.Module):
             kernel_init=nn.initializers.normal(),
             bias_init=nn.initializers.constant(0.0),
         )(x)
-            
+
         outputs = nn.Dropout(
             rate=self.config["dropout_rate"],
         )(x, deterministic=False)
@@ -42,41 +45,38 @@ class MLPBlock(nn.Module):
 
 
 class Encoder1DBlock(nn.Module):
-  """Transformer encoder layer.
+    """Transformer encoder layer.
 
-  Attributes:
-    config: TransformerConfig dataclass containing hyperparameters.
-  """
-  config: dict
-
-  @nn.compact
-  def __call__(self,
-               inputs,
-               mask=None):
-    """Applies Encoder1DBlock module.
-
-    Args:
-      inputs: input data.
-      mask: self-attention mask.
-
-    Returns:
-      output after transformer encoder block.
+    Attributes:
+      config: TransformerConfig dataclass containing hyperparameters.
     """
-    config = self.config
 
-    # Attention block.
-    x = nn.LayerNorm()(inputs)
-    x = nn.SelfAttention(
-        num_heads=config.num_heads,
-        qkv_features=config.qkv_dim)(x, mask)
+    config: dict
 
-    x = nn.Dropout(rate=config.dropout_rate)(
-        x, deterministic=config.deterministic)
-    x = x + inputs
+    @nn.compact
+    def __call__(self, inputs, mask=None):
+        """Apply Encoder1DBlock module.
 
-    # MLP block.
-    y = nn.LayerNorm()(x)
-    y = MLPBlock(config=config)(y)
+        Args:
+          inputs: input data.
+          mask: self-attention mask.
 
-    return x + y
+        Returns:
+          output after transformer encoder block.
+        """
+        config = self.config
 
+        # Attention block.
+        x = nn.LayerNorm()(inputs)
+        x = nn.SelfAttention(num_heads=config.num_heads, qkv_features=config.qkv_dim)(
+            x, mask
+        )
+
+        x = nn.Dropout(rate=config.dropout_rate)(x, deterministic=config.deterministic)
+        x = x + inputs
+
+        # MLP block.
+        y = nn.LayerNorm()(x)
+        y = MLPBlock(config=config)(y)
+
+        return x + y
