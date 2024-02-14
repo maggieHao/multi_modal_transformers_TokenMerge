c6bec909b1af1981e4e727dd9ae00ae22ff1011a
peterdavidfagan <peterdavidfagan@gmail.com>
2024-02-14 16:23:56 +0000

    basic t5 encoder

---
+++ multi_modal_transformers/tokenizers/text/t5_base.py
@@ -0,0 +1,31 @@
+import jax
+import jax.numpy as jnp
+from flax import linen as nn
+
+from transformers import FlaxT5EncoderModel, AutoTokenizer, AutoConfig
+
+
+class T5Tokenizer(nn.Module):
+
+    def setup(self):
+        self.config = AutoConfig.from_pretrained('t5-base')
+        self.model = FlaxT5EncoderModel(self.config).module
+
+    def __call__(self, input_ids):
+        return self.model(input_ids).last_hidden_state
+
+if __name__ == "__main__":
+     
+    instructions = [
+            "Pick up the red block and put it on the green block",
+            "Pick up the green block and put it on the red block",
+            ]
+
+    # tokenize instructions
+    tokenizer = AutoTokenizer.from_pretrained('t5-base')
+    inputs = tokenizer(instructions, return_tensors="jax", padding=True, truncation=True)
+    
+    # encode instructions
+    T5Tokenizer = T5Tokenizer()
+    params = T5Tokenizer.init(jax.random.PRNGKey(0), inputs['input_ids'])
+    outputs = T5Tokenizer.apply(params, inputs['input_ids'])
